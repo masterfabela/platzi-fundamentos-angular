8aecc984dd649c67ab534444907b1815cca4ee68
Jose Ramon Vieites <jose.vieites@altia.es>
2022-02-19 03:53:18 +0100

    Clase 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'platzi-fundamentos-angular';
+  name = 'Jose Ramon';
+  age = 18;
+  img =
+    'http://speed-new.com/wp-content/uploads/2015/07/2645457246456232546456457.jpg';
 }
