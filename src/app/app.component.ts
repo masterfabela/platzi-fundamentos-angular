e382a7cd2b41a0796534e0f88f7dddd7902d4573
Jose Ramon Vieites <jose.vieites@altia.es>
2022-02-19 04:06:47 +0100

    Property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,12 @@ export class AppComponent {
   age = 18;
   img =
     'http://speed-new.com/wp-content/uploads/2015/07/2645457246456232546456457.jpg';
+  btnDisabled = true;
+  inputValue = this.name;
+  person = {
+    name: 'nicolas',
+    age: 18,
+    avatar:
+      'https://www.happy-landing.net/wp-content/uploads/2020/09/avatar-2-tout-savoir.jpg',
+  };
 }
