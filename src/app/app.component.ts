2d58043721cd76da0624dc197d7525d35b1e4910
Jose Ramon Vieites <jose.vieites@altia.es>
2022-02-19 04:44:18 +0100

    Clase 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  name = 'Jose Ramon';
-  age = 18;
-  img =
-    'http://speed-new.com/wp-content/uploads/2015/07/2645457246456232546456457.jpg';
-  btnDisabled = true;
-  inputValue = this.name;
+  totalScroll = 100;
+  scrollValue = 0;
   person = {
     name: 'nicolas',
     age: 18,
     avatar:
       'https://www.happy-landing.net/wp-content/uploads/2020/09/avatar-2-tout-savoir.jpg',
   };
+  img =
+    'http://speed-new.com/wp-content/uploads/2015/07/2645457246456232546456457.jpg';
+  btnDisabled = true;
+  inputValue = this.person.age;
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  incrementAge() {
+    this.person.age++;
+  }
+
+  onBoxScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    this.totalScroll = element.scrollHeight - element.clientHeight;
+    this.scrollValue = element.scrollTop;
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
